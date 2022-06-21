7904165446bf494cf5effd581e660b1487fae6cb
darkopet <darkodepetrovski@gmail.com>
2022-06-21 16:50:04 +0200

    Database checks mysqli pdo

--- config.php
+++ config.php
@@ -5,7 +5,11 @@
     $password = "phpmyadmindb00+--+";
     $dbname= "phpcrud_simplelearn";
 
+    // $conn = new PDO('mysql:host=localhost;port=3306;dbname=phpcrud_simplelearn', 'phpmyadmin', 'phpmyadmindb00+--+');
+    # If the connection to the database is not succesfull:
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn = new mysqli($servername,$username,$password,$dbname);
+
     echo "<pre>";
     var_dump($conn);
     echo "</pre>";
